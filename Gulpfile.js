e2ce1f5f0e60109eca439d44c392d75b8601b172
Novica Pilipovic <novica.pilipovic@fincore.com>
2015-12-07 17:49:48 +0100

    The right way to write watchers {START}

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,17 +3,62 @@
  */
 
 var gulp = require('gulp'),
+    config = require('./gulp.config')(),
     gutil = require('gulp-util'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    server = require('gulp-express'),
+    ts = require('gulp-typescript'),
+    sourcemaps = require('gulp-sourcemaps'),
+    tslint = require('gulp-tslint'),
+    tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('ts-lint', function() {
+    return gulp.src(config.allTs)
+        .pipe(tslint())
+        .pipe(tslint.report('prose', {
+            emitError: false
+        }));
+});
+
+gulp.task('scripts', function() {
+    var sourceTsFiles = [
+        config.allTs,
+        config.typings
+    ];
+    
+    var tsResult = gulp.src(sourceTsFiles)
+                    .pipe(sourcemaps.init())
+                    .pipe(ts(tsProject));
+                    
+    return tsResult.js
+                  .pipe(sourcemaps.write('.'))
+                  .pipe(gulp.dest(config.tsOutputPath));
+                  
+                //   https://www.youtube.com/watch?v=7xOubdqyqaY
+});
 
 gulp.task('jshint', function() {
-    return gulp.src('/src/**/*.js')
+    return gulp.src('src/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('watch', function() {
-   gulp.watch('/src/**/*.js', ['jshint']);
+    gulp.watch('src/**/*.html', server.notify);
+    gulp.watch('src/**/*.css', function(event) {
+       server.notify(event); 
+    });
+    gulp.watch('src/**/*.ts', ['scripts']);
+    gulp.watch('src/**/*.js', ['jshint']);
+    gulp.watch('src/**/*.js', ['server']);
+});
+
+gulp.task('server', function() {
+    server.run(['server.js']);
+});
+
+gulp.task('serve', ['server', 'scripts', 'jshint', 'watch'], function() {
+    return gutil.log('Gulp is running!');
 });
 
 gulp.task('default', ['watch'], function() {
---
+++ gulp.config.js
@@ -0,0 +1,9 @@
+module.exports = function() {
+	var config = {
+		allTs: './src/**/*.ts',
+		typings: './**/*.d.ts',
+		tsOutputPath: './src/'
+	};
+	
+	return config;
+}
